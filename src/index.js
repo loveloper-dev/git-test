6058219b2fa50e9e24686be8de25045ae8565f00
dbsosl <89000658+dbsosl@users.noreply.github.com>
2024-06-01 23:10:56 +0900

    suregitong: 조아요~
    따봉스

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import TodoListApp from "./components/TodoListApp.js";
 
 const el = document.getElementById("app");
 
+cosole.log("스레기통 조아용~~")
+
 const todoListApp = new TodoListApp(el);
 todoListApp.render();
